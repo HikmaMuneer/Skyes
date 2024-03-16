d93fd00ffb5e7487499450e73d6756aa1157dc46
HikmaMuneer <hikma.muneer@gmail.com>
2024-03-16 21:33:48 +0530

    Initial Commit

---
+++ SkyesWardrobe/SkyesWardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyesWardrobeApp.swift
+//  SkyesWardrobe
+//
+//  Created by Hikma Muneer on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct SkyesWardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
