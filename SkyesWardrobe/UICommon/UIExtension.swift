0f734b98fa3b130a3c9616bdc8dba23b81cfc12e
HikmaMuneer <hikma.muneer@gmail.com>
2024-03-17 16:19:37 +0530

    Splash and Login Screens

---
+++ SkyesWardrobe/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  SkyesWardrobe
+//
+//  Created by Hikma Muneer on 2024-03-17.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
---
+++ SkyesWardrobe/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  SkyesWardrobe
+//
+//  Created by Hikma Muneer on 2024-03-17.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
